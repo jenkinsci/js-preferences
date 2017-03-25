34cebb47490b4d735e19714a1198cea7a225c23b
Thorsten Scherler <scherler@gmail.com>
2017-03-25 01:55:04 +0100

    [master] add smoke testing to validate basic functionality

--- spec/preferences-spec.js
+++ spec/preferences-spec.js
@@ -1,6 +1,56 @@
 /**
  * Created by thorsten on 3/24/17.
  */
-describe("Preference smoke testing", function () {
 
+const preferencesArray = [{
+        key: 'runDetails.logView',
+        defaultValue: 'pipeline',
+        allowedValues: ['classic', 'pipeline'],
+    },
+    {
+        key: 'runDetails.pipeline.updateOnFinish',
+        defaultValue: 'default',
+        allowedValues: ['default', 'never'],
+    },
+    {
+        key: 'runDetails.pipeline.showPending',
+        defaultValue: 'default',
+        allowedValues: ['default', 'never'],
+    },
+    {
+        key: 'runDetails.pipeline.karaoke',
+        defaultValue: 'default',
+        allowedValues: ['default', 'never'],
+    },
+];
+
+describe("Preference smoke testing", function () {
+    const preference = require('../preferences/Preference');
+    it("returns undefined", function () {
+        const prop = preference.newPreference('xxx');
+        expect(prop.value).toBe(undefined);
+    })
+    it("returns defaultValue", function () {
+        const prop = preference.newPreference('xxx', 'aaa');
+        expect(prop.value).toBe('aaa');
+    })
+    it("returns allowedValues", function () {
+        const prop = preference.newPreference('xxx2', 'xxx', ['aaa', 'xxx']);
+        expect(prop.value).toBe('xxx');
+        expect(prop.getAllowedValues()).toEqual(['aaa', 'xxx']);
+    })
+});
+
+describe("Preferences smoke testing", function () {
+    const preferences = require('../preferences/Preferences');
+    const karaokeConfig = preferences.newPreferences(preferencesArray);
+    it("returns undefined", function () {
+        const prop = karaokeConfig.getPreference('xxx');
+        expect(prop).toBe(undefined);
+    })
+    it("returns defaultValue and allowedValues", function () {
+        const prop = karaokeConfig.getPreference('runDetails.logView');
+        expect(prop.value).toBe('pipeline');
+        expect(prop.getAllowedValues()).toEqual(['classic', 'pipeline']);
+    })
 });
